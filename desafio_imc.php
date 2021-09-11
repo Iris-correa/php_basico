26a15697299480dbd1f5afa728e3afe588c1dee2
iris-correa <iris555.correa@yahoo.com>
2021-09-11 13:15:04 -0300

    new_changes-reduction

--- desafio_imc.php
+++ desafio_imc.php
@@ -7,24 +7,33 @@ $altura = 1.59;
 
 $res = $peso /($altura*$altura);
 
+echo "Seu IMC é igual a $res kg/m2, ";
+
 if($res <= 16.9){
-    echo "Seu IMC é igual a $res kg/m2, está muito abaixo do peso" .PHP_EOL;
+    echo "está muito abaixo do peso" .PHP_EOL;
 }
+
 elseif( $res <= 18.4){
-    echo "Seu IMC é igual a $res kg/m2, está abaixo do peso" .PHP_EOL;
+    echo "está abaixo do peso" .PHP_EOL;
 }
+
 elseif( $res <= 24.9){
-    echo " Seu IMC é igual a $res kg/m2, está com peso normal " .PHP_EOL;
+    echo "está com peso normal " .PHP_EOL;
 }
+
 elseif( $res <= 29.9){
-    echo " Seu IMC é igual a $res kg/m2, está com peso acima " .PHP_EOL;
+    echo "está com peso acima " .PHP_EOL;
 }
+
 elseif( $res <= 34.9){
-    echo " Seu IMC é igual a $res kg/m2, está com obesidade grau l " .PHP_EOL;
+    echo "está com obesidade grau l " .PHP_EOL;
 }
+
 elseif( $res <= 40){
-    echo " Seu IMC é igual a $res kg/m2, está com obesidade grau ll " .PHP_EOL;
+    echo "está com obesidade grau ll " .PHP_EOL;
 }
+
 else{
-      echo " Seu IMC é igual a $res kg/m2, está com obesidade grau lll " .PHP_EOL;
+      echo "está com obesidade grau lll " .PHP_EOL;
 }
+
