85cf45e1b3dff7ed9f27823685082d4ddcabcaf8
Iris Correa <83985771+Iris-correa@users.noreply.github.com>
2021-09-11 12:56:59 -0300

    Update desafio_impar.php

--- desafio_impar.php
+++ desafio_impar.php
@@ -1,7 +1,8 @@
 <?php
 
-    for($ind=0;$ind<=10;$ind++){
-        {
+    for($ind=0;$ind<=100;$ind++){
+        
+        if($ind % 2 != 0){
             echo "$ind este número é impar!" .PHP_EOL;
         }
     }
